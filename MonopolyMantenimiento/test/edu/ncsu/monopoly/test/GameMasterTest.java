b91b39ce9f60af13890c1ef5f0d364a5891b5c0f
Guillermo Jude <guillejude@hotmail.com>
2017-05-06 16:24:40 -0300

    Se implementaron mas pruebas unitarias en la clase GameMaster.

--- MonopolyMantenimiento/test/edu/ncsu/monopoly/test/GameMasterTest.java
+++ MonopolyMantenimiento/test/edu/ncsu/monopoly/test/GameMasterTest.java
@@ -1,7 +1,5 @@
 package edu.ncsu.monopoly.test;
 
-
-
 import java.util.ArrayList;
 
 import edu.ncsu.monopoly.GameMaster;
@@ -16,36 +14,37 @@ import junit.framework.TestCase;
 
 public class GameMasterTest extends TestCase {
 
-	GameMaster gameMaster;
-	
-	protected void setUp() throws Exception {
-		gameMaster = GameMaster.instance();
-		gameMaster.setGameBoard(new GameBoardFull());
-		gameMaster.setNumberOfPlayers(2);
+    GameMaster gameMaster;
+
+    protected void setUp() throws Exception {
+        gameMaster = GameMaster.instance();
+        gameMaster.setGameBoard(new GameBoardFull());
+        gameMaster.setNumberOfPlayers(2);
+        gameMaster.setInitAmountOfMoney(1500);
         gameMaster.getPlayer(0).setName("Player 1");
         gameMaster.getPlayer(1).setName("Player 2");
-		gameMaster.reset();
-		gameMaster.setTestMode(true);
+        gameMaster.reset();
+        gameMaster.setTestMode(true);
         gameMaster.setGUI(new MockGUI());
         gameMaster.startGame();
-	}
-	
-	public void testInit() {
-		assertEquals(gameMaster.getInitAmountOfMoney(),
-				gameMaster.getPlayer(0).getMoney());
-	}
-	
-	public void testReset() {
-		gameMaster.movePlayer(0, 3);
-		gameMaster.movePlayer(1, 4);
-		gameMaster.reset();
-		for(int i = 0; i < gameMaster.getNumberOfPlayers(); i++) {
-			Player player = gameMaster.getPlayer(i);
-			assertEquals("Go", player.getPosition().getName());
-		}
-		assertEquals(0, gameMaster.getTurn());
-	}
-    
+    }
+
+    public void testInit() {
+        assertEquals(gameMaster.getInitAmountOfMoney(),
+                gameMaster.getPlayer(0).getMoney());
+    }
+
+    public void testReset() {
+        gameMaster.movePlayer(0, 3);
+        gameMaster.movePlayer(1, 4);
+        gameMaster.reset();
+        for (int i = 0; i < gameMaster.getNumberOfPlayers(); i++) {
+            Player player = gameMaster.getPlayer(i);
+            assertEquals("Go", player.getPosition().getName());
+        }
+        assertEquals(0, gameMaster.getTurn());
+    }
+
     public void testTradeProcess() {
         MonopolyGUI gui = gameMaster.getGUI();
         assertTrue(gui.isTradeButtonEnabled(0));
@@ -54,7 +53,7 @@ public class GameMasterTest extends TestCase {
         assertFalse(gui.isTradeButtonEnabled(0));
         assertFalse(gui.isTradeButtonEnabled(1));
         gameMaster.getCurrentPlayer().purchase();
-        assertEquals(gameMaster.getGameBoard().getCell(1),gameMaster.getCurrentPlayer().getAllProperties()[0]);
+        assertEquals(gameMaster.getGameBoard().getCell(1), gameMaster.getCurrentPlayer().getAllProperties()[0]);
         gameMaster.btnEndTurnClicked();
         TradeDialog dialog = gui.openTradeDialog();
         assertEquals(1, gameMaster.getNumberOfSellers());
@@ -71,38 +70,50 @@ public class GameMasterTest extends TestCase {
         assertFalse(player1.checkProperty(deal.getPropertyName()));
         assertTrue(player2.checkProperty(deal.getPropertyName()));
     }
-	
-	public void testTurn() {
-		assertEquals(0, gameMaster.getTurn());
-		gameMaster.switchTurn();
-		assertEquals(1, gameMaster.getTurn());
-		gameMaster.switchTurn();
-		assertEquals(0, gameMaster.getTurn());
-	}
-	
-	
-	
-	public void testButtonPurchasePropertyClicked() {
-		gameMaster.movePlayer(0,1);
-		gameMaster.btnPurchasePropertyClicked();
-		assertEquals(gameMaster.getGameBoard().getCell(1), gameMaster.getCurrentPlayer().getAllProperties()[0]);
-		assertEquals(1440,gameMaster.getCurrentPlayer().getMoney());
-	}
-	
-	public void testButtonRollDiceClicked() {
-		gameMaster.reset();
-		gameMaster.btnRollDiceClicked();
-		assertEquals(0,gameMaster.getCurrentPlayerIndex());
-		assertEquals(gameMaster.getGameBoard().getCell(5), gameMaster.getPlayer(0).getPosition());
-	}
-	
-	public void testButtonTradeClicked() {
-		gameMaster.movePlayer(0,1);
-		gameMaster.getCurrentPlayer().purchase();
-		gameMaster.btnEndTurnClicked();
-		gameMaster.btnTradeClicked();
-		assertEquals(gameMaster.getGameBoard().getCell(1),gameMaster.getCurrentPlayer().getAllProperties()[0]);
-		assertEquals(1640,gameMaster.getPlayer(0).getMoney());
-		assertEquals(1300,gameMaster.getPlayer(1).getMoney());
-	}
+
+    public void testTurn() {
+        assertEquals(0, gameMaster.getTurn());
+        gameMaster.switchTurn();
+        assertEquals(1, gameMaster.getTurn());
+        gameMaster.switchTurn();
+        assertEquals(0, gameMaster.getTurn());
+    }
+
+    public void testButtonPurchasePropertyClicked() {
+        gameMaster.movePlayer(0, 1);
+        gameMaster.btnPurchasePropertyClicked();
+        assertEquals(gameMaster.getGameBoard().getCell(1), gameMaster.getCurrentPlayer().getAllProperties()[0]);
+        assertEquals(1440, gameMaster.getCurrentPlayer().getMoney());
+    }
+
+    public void testButtonRollDiceClicked() {
+        gameMaster.reset();
+        gameMaster.btnRollDiceClicked();
+        assertEquals(0, gameMaster.getCurrentPlayerIndex());
+        assertEquals(gameMaster.getGameBoard().getCell(5), gameMaster.getPlayer(0).getPosition());
+    }
+
+    public void testButtonTradeClicked() {
+        gameMaster.movePlayer(0, 1);
+        gameMaster.getCurrentPlayer().purchase();
+        gameMaster.btnEndTurnClicked();
+        gameMaster.btnTradeClicked();
+        assertEquals(gameMaster.getGameBoard().getCell(1), gameMaster.getCurrentPlayer().getAllProperties()[0]);
+        assertEquals(1640, gameMaster.getPlayer(0).getMoney());
+        assertEquals(1300, gameMaster.getPlayer(1).getMoney());
+    }
+    
+    public void testBtnGetOutOfJailClicked(){
+        gameMaster.getCurrentPlayer().setInJail(true);
+        gameMaster.btnGetOutOfJailClicked();
+        assertFalse(gameMaster.getGUI().isGetOutOfJailButtonEnabled());
+    }
+    
+    public void testBtnGetOutOfJailClickedBankrupt(){
+        gameMaster.getCurrentPlayer().setInJail(true);
+        gameMaster.getCurrentPlayer().setMoney(0);
+        gameMaster.btnGetOutOfJailClicked();
+        assertFalse(gameMaster.getCurrentPlayer().isInJail());
+    }
+    
 }
